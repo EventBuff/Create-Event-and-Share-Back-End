00dd45e3637602d95c52f53e13e4065f71343215
Lich Amnesia <alwaysxiaop@gmail.com>
2016-11-06 22:52:03 -0700

    update Profile component

--- my-app/src/Profile.js
+++ my-app/src/Profile.js
@@ -2,7 +2,7 @@
 * @Author: Lich Amnesia
 * @Date:   2016-11-06 21:56:58
 * @Last Modified by:   Lich Amnesia
-* @Last Modified time: 2016-11-06 22:08:43
+* @Last Modified time: 2016-11-06 22:26:57
 */
 
 
@@ -11,7 +11,7 @@ import React, { Component } from 'react';
 import './Profile.css';
 // import Navigation from './Navigation';
 import axios from 'axios';
-import { Button, Row, Col}
+import { Button, Row, Col, ListGroup, ListGroupItem}
   from 'react-bootstrap';
 import { Link } from 'react-router';
 
@@ -47,7 +47,11 @@ class Profile extends Component {
     } else {
       var content = 
           <div>
-            <h4>{data.eventtitle}</h4>
+            <h4> {data.email} </h4>
+            <p> {data.gender} </p>
+            <p> {data.firstname} </p>
+            <p> {data.lastname} </p>
+            <p> {data.phonenumber} </p>
           </div>;
       this.setState({ 
           posts: content,
@@ -59,13 +63,18 @@ class Profile extends Component {
   render() {
     var value = this.state.value;
     var posts = this.state.posts;
+    // list groun need to changed for editting
     return (
       <div>
         <div>
           <Row>
             <Col className="ProfileNavigation" xs={12} md={3}>
-            Tags:
-              
+              <p> Profile </p>
+              <ListGroup>
+                <ListGroupItem active>Check Profile</ListGroupItem>
+                <ListGroupItem>Event History</ListGroupItem>
+                <ListGroupItem>Edit Profile</ListGroupItem>
+              </ListGroup>  
             </Col>
             <Col md={9}>
               {posts}
